edc441e4de5a47543c3d82a0b581af37967af2f5
Mike Cann <mike.cann@gmail.com>
2018-04-07 20:12:43 +0800

    fixing the css name issue

--- renderer/src/App.tsx
+++ renderer/src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import './app.css';
+import './App.css';
 import ManualProfileSwitcher from './widgets/manual-profile-switcher/ManualProfileSwitcher';
 import { wrap } from './utils';
 import { CSSTransitionGroup } from 'react-transition-group';
