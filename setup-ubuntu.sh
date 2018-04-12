7bf1b1c9fb169627ea40997daca0b2b384ddd4eb
Mike Cann <mike.cann@gmail.com>
2018-04-12 14:51:53 +0800

    added ubuntu setup sh

---
+++ setup-ubuntu.sh
@@ -0,0 +1,11 @@
+# Install NPM
+sudo apt-get update
+sudo apt-get install nodejs
+sudo apt-get install npm
+nodejs -v
+
+# Install Yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update
+sudo apt-get install yarn
