f15ce9417ac9ca98a2df0b0cd642dcf0be2ae2eb
Mike Cann <mike.cann@gmail.com>
2018-04-12 14:54:30 +0800

    added repo to sh

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,11 +1,13 @@
 # Install NPM
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get update
-sudo apt-get install nodejs
-sudo apt-get install npm
+sudo apt-get install nodejs -y
+sudo apt-get install npm -y
+sudo apt-get install -y build-essential
 nodejs -v
 
 # Install Yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update
-sudo apt-get install yarn
+sudo apt-get update 
+sudo apt-get install yarn -y
