6d03b414830259578738c6f4dddd4d5bd21fa2cb
Mike Cann <mike.cann@gmail.com>
2018-04-19 13:08:14 +0800

    .

--- cron.sh
+++ cron.sh
@@ -2,8 +2,8 @@
 changed=0
 git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
 if [ $changed = 1 ]; then
-    echo "Update needed, running..";
+    echo "Update needed, running.."
     git pull
     yarn install
-    echo "Updated successfully";
+    echo "Updated successfully"
 fi
