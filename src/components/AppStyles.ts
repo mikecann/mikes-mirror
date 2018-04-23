5e8248519ce35ec74b2182bb6ce32a4b976a94e5
Mike Cann <mike.cann@gmail.com>
2018-04-23 08:20:59 +0800

    adding styles to the right

--- src/components/AppStyles.ts
+++ src/components/AppStyles.ts
@@ -1,35 +1,3 @@
-// .app {
-//     width: calc(100% - 30px);
-//     height: calc(100% - 20px);
-//     padding-left: 10px;
-//     position: relative;
-//     cursor: none;
-// }
-
-// .profile {
-//     padding: 100px;
-// }
-
-
-// .profile-enter {
-//     opacity: 0.01;
-// }
-
-// .profile-enter.profile-enter-active {
-//     opacity: 1;
-//     transition: opacity 500ms ease-in;
-// }
-
-// .profile-leave {
-//     opacity: 1;
-// }
-
-// .profile-leave.profile-leave-active {
-//     opacity: 0.01;
-//     transition: opacity 300ms ease-in;
-// }
-
-
 import { stylesheet, cssRule } from "typestyle/lib";
 
 cssRule(".profile-enter", {
