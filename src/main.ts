0bc216c647d35faa1cfdeb148bd54aa9a87cf04a
Mike Cann <mike.cann@gmail.com>
2018-05-03 21:12:23 +0800

    hide mouse

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import * as path from "path";
 import * as url from "url";
 import * as reload from "electron-reload";
 import { waitForUpdate } from './utils/GitBasedAutoUpdater';
+import { exec } from "child_process";
 
 // Work out if we are running in prod mode or not
 const isProduction = process.env.NODE_ENV == "production";
@@ -31,11 +32,14 @@ app.on("ready", async () => {
   }));
 
   // If we are dev mode lets open the dev tools
-  //if (isDev)
+  if (isDev)
     window.webContents.openDevTools();
 
   // Prevent the display from sleeping
   powerSaveBlocker.start("prevent-display-sleep");
+
+  // Lets move the mouse out the way if we can
+  hideMouse();  
 
   // After 2 hours lets manually restart, just incase..
   setTimeout(() => window.close, 2 * 3600 * 1000);
@@ -63,5 +67,10 @@ const createWindowDev = () => new BrowserWindow({
   // webPreferences: {
   //   webSecurity: false
   // }
+});
+
+const hideMouse = () => exec("xdotool mousemove 0 30000", (err, stdout, stderr) => {
+  if (err) 
+      return console.error("Canot move the mouse out the way, its not a biggie, will continue on anways.. ", err);
 });
 
